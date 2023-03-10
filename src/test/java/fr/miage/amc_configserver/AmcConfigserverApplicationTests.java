3651179e862b1c508f951a95360eb9ed59b8c6a9
Patrice Torguet <patrice.torguet@irit.fr>
2023-03-10 17:25:59 +0100

    Initial commit

---
+++ src/test/java/fr/miage/amc_configserver/AmcConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.amc_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmcConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
